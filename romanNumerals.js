22e703b86e1ec669cf6f64a66f6c8eb67601758f
Jkim1122 <jkimz1122@gmail.com>
2023-10-04 21:35:11 -0500

    done - toRomanLazy, not complete - toRoman

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,6 +1,33 @@
 function toRomanLazy(num) {
-  return "";
+  let result = "";
+  const romanNumeralToArabic = {
+      "M": 1000,
+      "CM": 900,
+      "D": 500,
+      "C": 100,
+      "XC": 90,
+      "L": 50,
+      "X": 10,
+      "IX": 9,
+      "V": 5,
+      "I": 1,
+  }
+  for (const key in romanNumeralToArabic){
+    const numberValue = romanNumeralToArabic[key];
+
+    while (num >= romanNumeralToArabic[key]){
+      num -= numberValue
+      result += key
+    }
+    // const numValue = romanNumeralToArabic[key];
+  }
+  return result;
 }
+
+console.log(toRomanLazy(4))
+console.log(toRomanLazy(150))
+console.log(toRomanLazy(944))
+
 
 function toRoman(num) {
   return "";
